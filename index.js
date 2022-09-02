c1b802d024e5920912df4271c24fa29ffb40eba4
Tess <tere0506@stud.kea.dk>
2022-09-02 14:35:23 +0200

    data Handling

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 "use strict";
+// Initialize
+document.addEventListener("DOMContentLoaded", getValues);
+
 //Variables
-const queueSize = getNumberOfCustomers();
 const queueList = document.querySelector("ul");
 const totalValue = 32;
-
-let arrayObject;
-let createList;
-let value;
-let newValue;
-
-//////////////  Data Handleing  /////////////
-
-// Fake Data
-function getNumberOfCustomers() {
-  //FAKE: gives a completely random number
-  return Math.floor(Math.random() * 32);
-}
-
-//////////////  Visuals  /////////////
 const arrayQueue = [
   "10",
   "15",
@@ -57,8 +44,41 @@ const arrayQueue = [
   "11",
   "5",
   "9",
+  "7",
+  "15",
 ];
-createLi();
+
+let arrayObject;
+let arrayLength;
+let createList;
+let value;
+let newValue;
+
+//////////////  Data Handleing  /////////////
+
+// Fake Data
+function getNumberOfCustomers() {
+  //FAKE: gives a completely random number
+  return Math.floor(Math.random() * 32);
+}
+
+function getValues() {
+  arrayLength = arrayQueue.length;
+  modArray();
+}
+
+function modArray() {
+  const queueSize = getNumberOfCustomers();
+  console.log("here");
+  arrayQueue.push(`${queueSize}`);
+  console.log(arrayQueue);
+  if (arrayQueue.length > 40) {
+    arrayQueue.shift();
+    console.log(arrayQueue);
+  }
+}
+
+//////////////  Visuals  /////////////
 
 function createLi() {
   for (let i = 0; i <= arrayQueue.length; i++) {
@@ -85,3 +105,82 @@ function translateValue() {
   createList.style.transform = `translate(0,-${newValue}%)`;
   console.log(createList.value);
 }
+
+/* //Variables
+const queueList = document.querySelector("ul");
+const totalValue = 32;
+const arrayQueue = [
+  "32",
+  "21",
+  "10",
+  "19",
+  "26",
+  "21",
+  "26",
+  "27",
+  "6",
+  "3",
+  "7",
+  "10",
+  "9",
+  "10",
+  "14",
+  "3",
+  "28",
+  "0",
+  "11",
+  "32",
+  "25",
+  "19",
+  "18",
+  "6",
+  "4",
+  "3",
+  "7",
+  "28",
+  "4",
+  "32",
+  "12",
+  "6",
+  "8",
+  "1",
+  "21",
+  "12",
+  "5",
+  "13",
+  "6",
+  "30",
+];
+
+let arrayObject;
+let arrayLength;
+let createList;
+let value;
+let newValue;
+
+//////////////  Data Handleing  /////////////
+
+// Fake Data
+function getNumberOfCustomers() {
+  //FAKE: gives a completely random number
+  return Math.floor(Math.random() * 32);
+}
+
+function getValues() {
+  arrayLength = arrayQueue.length;
+  modArray();
+}
+
+function modArray() {
+  const queueSize = getNumberOfCustomers();
+  console.log("here");
+  arrayQueue.push(`${queueSize}`);
+  console.log(arrayQueue);
+  if (arrayQueue.length > 40) {
+    arrayQueue.shift();
+    console.log(arrayQueue);
+    setTimeout(modArray, 1000);
+    createLi();
+  }
+  removeLi();
+} */
