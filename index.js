8bd3f9ddc1932d63732f49bb2ed9c6b8f0b7be7e
Tess <tere0506@stud.kea.dk>
2022-09-02 15:14:46 +0200

    js cleanup

--- index.js
+++ index.js
@@ -5,48 +5,7 @@ document.addEventListener("DOMContentLoaded", startLoop);
 //Variables
 const queueList = document.querySelector("ul");
 const totalValue = 32;
-const arrayQueue = [
-  // "10",
-  // "15",
-  // "27",
-  // "29",
-  // "29",
-  // "12",
-  // "13",
-  // "7",
-  // "29",
-  // "14",
-  // "9",
-  // "27",
-  // "7",
-  // "22",
-  // "8",
-  // "3",
-  // "27",
-  // "25",
-  // "21",
-  // "15",
-  // "27",
-  // "32",
-  // "3",
-  // "26",
-  // "24",
-  // "4",
-  // "3",
-  // "11",
-  // "18",
-  // "29",
-  // "19",
-  // "21",
-  // "12",
-  // "17",
-  // "14",
-  // "11",
-  // "5",
-  // "9",
-  // "7",
-  // "15",
-];
+const arrayQueue = [];
 
 let arrayObject;
 let arrayLength;
